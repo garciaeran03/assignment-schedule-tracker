cdde4aee1b899ddd32803799a65a945f6f4b3711
garciaeran03 <garcia.eran03@gmail.com>
2026-09-17 12:20:15 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,5 @@
-const SUPABASE_URL = "https://mukfujnqbmtlhexzvway.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://mukfujnqbmtlhexzvway.supabase.co";
+
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_HY5ixDdIar8ESW0u8dOpKA_HD6JVY0G";
 
 const supabaseClient = window.supabase.createClient(
