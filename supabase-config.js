9da54e0fe43ae464664bd279938de7093ba313d5
garciaeran03 <garcia.eran03@gmail.com>
2026-09-17 12:28:34 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://mukfujnqbmtlhexzvway.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11a2Z1am5xYm10bGhleHp2d2F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2MTEyMjgsImV4cCI6MjEwNTE4NzIyOH0.sRUPOWYQ6hYj7NVNK4E8i-XTBDk5zdx8gLLLSUfSs3A";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_HY5ixDdIar8ESW0u8dOpKA_HD6JVY0G";
 
 window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
