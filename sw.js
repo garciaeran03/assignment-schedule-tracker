aa0c7c77a2d2eab087b2a15195f5a9771aba0c25
garciaeran03 <garcia.eran03@gmail.com>
2026-09-17 13:45:50 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,84 @@
+const CACHE_NAME = "school-tracker-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./supabase-config.js",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+
+// INSTALL
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(APP_FILES);
+            })
+    );
+
+    self.skipWaiting();
+});
+
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+
+                return Promise.all(
+                    cacheNames
+                        .filter(cacheName => cacheName !== CACHE_NAME)
+                        .map(cacheName => caches.delete(cacheName))
+                );
+
+            })
+    );
+
+    self.clients.claim();
+});
+
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+    const request = event.request;
+
+    // Do not cache Supabase/API requests
+    if (
+        request.url.includes("supabase.co") ||
+        request.method !== "GET"
+    ) {
+        return;
+    }
+
+    event.respondWith(
+
+        fetch(request)
+            .then(response => {
+
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(request, responseClone);
+                    });
+
+                return response;
+
+            })
+            .catch(() => {
+
+                return caches.match(request);
+
+            })
+
+    );
+});
